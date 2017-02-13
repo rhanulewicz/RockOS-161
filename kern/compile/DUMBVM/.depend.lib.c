4337ef4207681af33da5b3c93a90c9ce7de054c7
rshanule <rshanule@buffalo.edu>
2017-02-13 19:21:17 +0000

    uuuuhhhhhh

--- kern/compile/DUMBVM/.depend.lib.c
+++
@@ -1,8 +0,0 @@
-lib.o: ../../test/lib.c ../../include/types.h ../../include/kern/types.h \
- includelinks/kern/machine/types.h includelinks/machine/types.h \
- ../../include/thread.h ../../include/array.h ../../include/cdefs.h \
- ../../include/lib.h opt-noasserts.h ../../include/spinlock.h \
- includelinks/machine/spinlock.h ../../include/threadlist.h \
- includelinks/machine/thread.h ../../include/setjmp.h \
- includelinks/kern/machine/setjmp.h ../../include/test.h \
- ../../include/kern/secret.h opt-synchprobs.h opt-automationtest.h
