f3a1736f605c6a5d42ef763b1bc7af138094c965
searay330 <davidstech99@gmail.com>
2017-03-10 23:34:12 -0500

    most of exec done just need to do args not sure how to get them in

--- kern/arch/mips/syscall/syscall.c
+++ kern/arch/mips/syscall/syscall.c
@@ -429,6 +429,59 @@ int execv(const char *program, char **args, int32_t *retval){
 	(void)args;
 	(void)retval;
 
+	char * buffer = kmalloc(sizeof(*args));
+	copyin((const_userptr_t)*args, buffer, sizeof(*args));
+	as_deactivate();
+	// struct addrspace *old = proc_setas(NULL);
+	proc_setas(NULL);
+	struct addrspace *as;
+	struct vnode *v;
+	vaddr_t entrypoint, stackptr;
+	int result;
+
+	/* Open the file. */
+	result = vfs_open((char*) program, O_RDONLY, 0, &v);
+	if (result) {
+		return result;
+	}
+
+	/* We should be a new process. */
+	KASSERT(proc_getas() == NULL);
+
+	/* Create a new address space. */
+	as = as_create();
+	if (as == NULL) {
+		vfs_close(v);
+		return ENOMEM;
+	}
+
+	/* Switch to it and activate it. */
+	proc_setas(as);
+	as_activate();
+
+	/* Load the executable. */
+	result = load_elf(v, &entrypoint);
+	if (result) {
+		/* p_addrspace will go away when curproc is destroyed */
+		vfs_close(v);
+		return result;
+	}
+
+	/* Done with the file now. */
+	vfs_close(v);
+
+	/* Define the user stack in the address space */
+	result = as_define_stack(as, &stackptr);
+	if (result) {
+		/* p_addrspace will go away when curproc is destroyed */
+		return result;
+	}
+
+	/* Warp to user mode. */
+	enter_new_process(0 /*argc*/, NULL /*userspace addr of argv*/,
+			  NULL /*userspace addr of environment*/,
+			  stackptr, entrypoint);
+
 	return 0;
 }
 
